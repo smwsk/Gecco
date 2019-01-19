d07de6efcd7d5a376dae52cb2318ed780bf16cb1
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-01-19 12:31:51 +0800

    项目初始化

---
+++ gecco-consumer/src/main/java/com/smu/geccoconsumer/GeccoConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.smu.geccoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeccoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeccoConsumerApplication.class, args);
+    }
+
+}
+
